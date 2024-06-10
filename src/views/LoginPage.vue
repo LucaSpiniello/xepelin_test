c81fc828ad1163868990da50977dac6a773b7584
Luca Figari <luca.figari@uc.cl>
2024-06-10 14:12:25 +0100

    firebase integration without prints

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -15,17 +15,6 @@
 import { auth } from '@/firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 
-const firebaseConfig = {
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VUE_APP_FIREBASE_APP_ID,
-    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
-};
-console.log(firebaseConfig)
-
 export default {
   name: 'LoginPage',
   data() {
